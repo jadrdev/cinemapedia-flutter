02455c728f9f65dd526e670e133cdaff58ed3da1
Joshua A. Díaz Robayna <jrobayna@velocitech.io>
2023-10-10 13:13:18 +0100

    Creacion de la ruta hija

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,12 +2,21 @@ import 'package:cinemapedia/presentacion/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/', 
+  initialLocation: '/',
   routes: [
     GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
+        path: '/',
+        name: HomeScreen.name,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: 'movie/:id',
+            name: MovieScreen.name,
+            builder: (context, state) {
+              final movieID = state.pathParameters['id'] ?? 'no-id';
+              return MovieScreen(movieid: movieID);
+            },
+          ),
+        ]),
   ],
 );
