dcf33d40430b6040b4b8f5b0b14a9346f747618e
Joshua A. Díaz Robayna <jrobayna@velocitech.io>
2023-10-10 10:42:30 +0100

    Create Providers

--- lib/presentacion/providers/movies/movies_providers.dart
+++ lib/presentacion/providers/movies/movies_providers.dart
@@ -8,19 +8,42 @@ final nowPlayingMoviesProvider =
   return MoviesNotifier(fectchMoreMovies: fecthMoreMovies);
 });
 
+final popularMovieProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fecthMoreMovies = ref.watch(movieRepositoryProvider).getPopular;
+  return MoviesNotifier(fectchMoreMovies: fecthMoreMovies);
+});
+
+final topRatedMovieProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fecthMoreMovies = ref.watch(movieRepositoryProvider).getTopRated;
+  return MoviesNotifier(fectchMoreMovies: fecthMoreMovies);
+});
+
+final upComingMovieProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fecthMoreMovies = ref.watch(movieRepositoryProvider).getUpComing;
+  return MoviesNotifier(fectchMoreMovies: fecthMoreMovies);
+});
+
 typedef MovieCallback = Future<List<Movie>> Function({int page});
 
 class MoviesNotifier extends StateNotifier<List<Movie>> {
   int currentPage = 0;
+  bool isloading = false;
   MovieCallback fectchMoreMovies;
   MoviesNotifier({
     required this.fectchMoreMovies,
   }) : super([]);
 
   Future<void> loadNextPage() async {
+    if (isloading) return;
+    isloading = true;
     currentPage++;
 
     final List<Movie> movies = await fectchMoreMovies(page: currentPage);
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 500));
+    isloading = false;
   }
 }
